db809e5922ff4ec0bfa6024f57305de226124a28
Yasuhito Fukuda <fukuda.yasuhito@po.ntts.co.jp>
2015-09-15 17:20:09 +0900

    add combination scinario 1-4,1-5,2-1

---
+++ example/zk.byteman.add_node/materials/1-5/validate.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -x
+
+sleep 10
+bash $EQ_MATERIALS_DIR/check-fle-states.sh 2181 2182 2183
+$EQ_MATERIALS_DIR/zookeeper/bin/zkCli.sh -server localhost:2181,localhost:2182,localhost:2183 create /hoo bar
+
